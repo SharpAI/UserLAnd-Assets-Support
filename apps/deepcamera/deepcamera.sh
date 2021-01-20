2cf1fb02decfbc53cbf248b4f61cb091376936a5
Simba Zhang <solderzzc@users.noreply.github.com>
2021-01-19 17:08:10 -0800

    Update deepcamera.sh

--- apps/deepcamera/deepcamera.sh
+++ apps/deepcamera/deepcamera.sh
@@ -2,4 +2,7 @@ SCRIPT_PATH=$(realpath ${BASH_SOURCE})
 
 sudo rm -f $SCRIPT_PATH
 
-echo "Welcome to Debian in UserLAnd!"
+echo "Welcome to DeepCamera on Android!"
+
+sudo bash /runtime/scripts/start.sh
+
